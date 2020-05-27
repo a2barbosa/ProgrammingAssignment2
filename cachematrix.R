02ffed1ba67560b01d161288b309737edc402812
a2barbosa <a2barbosa@gmail.com>
2020-05-27 18:46:39 -0300

    Final changes in my Programming Assignment 2 at Coursera R Programming course.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,42 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set,
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then cacheSolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("cached inverse matrix found, getting the matrix...")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
 }
+
+# Testing
+# special_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+# cacheSolve(special_matrix)
+
+# Computing the inverse of a square matrix can be done with the solve function in R.
+# For example, if X is a square invertible matrix, then solve(X) returns its inverse.
